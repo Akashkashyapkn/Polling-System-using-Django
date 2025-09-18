801d7e8eae024025b7bd576278c5bb54f803074e
AKASH KASHYAP KN <159678949+Akashkashyapkn@users.noreply.github.com>
2025-09-18 12:26:52 +0530

    Update settings.py

--- pollme/settings.py
+++ pollme/settings.py
@@ -100,6 +100,8 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+ALLOWED_HOSTS = ['akashdjango.azurewebsites.net']
+
 
 
 # Internationalization
